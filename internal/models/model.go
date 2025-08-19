15eb72dbd5cd47e6b88966fe818bb8d1ab3c8d72
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-08-20 01:36:16 +0600

    snake_case error : add gorm  column name for SID

--- internal/models/model.go
+++ internal/models/model.go
@@ -2,7 +2,7 @@ package models
 
 type Users struct {
 	ID   int     `gorm:"primaryKey;autoIncrement" json:"id"`
-	SID  string  `json:"sid"`
+	SID  string  `gorm:"column:sid" json:"sid"`
 	Name string  `json:"name"`
 	CGPA float32 `json:"cgpa"`
 }
